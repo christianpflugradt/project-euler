2c8a10c97ccdf2b61dcd44b6d628817b6ee94c94
Christian Pflugradt <christian@pflugradts.de>
2023-03-12 13:35:15 +0100

    solved problem 010 in haskell

---
+++ solutions-haskell/010.hs
@@ -0,0 +1,13 @@
+sqrt' :: Int -> Float
+sqrt' n = sqrt( fromIntegral n )
+
+isPrime :: Int -> Bool
+isPrime 2 = True
+isPrime n
+    | n < 2 = False
+    | ( length [ x | x <- [ 2 .. ceiling( sqrt' n ) ], mod n x == 0 ] ) > 0 = False
+    | otherwise = True
+
+main :: IO()
+main = do
+    print( sum( [ x | x <- [ 2 .. 1999999 ], isPrime x ] ) )
